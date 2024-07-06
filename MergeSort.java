20e2c38519c5a73aa8dcbc081272811f700dd3a2
Usha Ramchandra Kudale <129097741+Ushakudale1511@users.noreply.github.com>
2024-07-06 21:55:30 +0530

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -15,19 +15,19 @@ public class MergeSort {
         }
     }
     public static void merge(int[] arr,int low,int middle,int high ){
-        //用于存储归并后的临时数组
+        
         int[] temp = new int[high-low+1];
-        //用于记录第一个数组中需要遍历的下标
+       
         int i = low;
-        //用于记录第二个数组中需要遍历的下标
+        
         int j = middle+1;
-        //用于记录在临时数组中存放的下标
+        
         int index= 0;
-        //遍历两个数组取出小的数字放入临时数组中
+        
         while(i<=middle&&j<=high){
-            //第一个数组的数更小
+          
             if(arr[i]<arr[j]){
-                //把小的数放进临时数组里
+                
                 temp[index]=arr[i];
                 i++;
             }
@@ -37,7 +37,7 @@ public class MergeSort {
             }
             index++;
         }
-        //把剩余的数组放入临时数组;
+        
         while(j<=high){
             temp[index]=arr[j];
             j++;
@@ -48,7 +48,7 @@ public class MergeSort {
             i++;
             index++;
         }
-        //把临时数组里的数据放入原数组
+        
         for(int k=0;k<temp.length;k++){
             arr[k+low]=temp[k];
         }
